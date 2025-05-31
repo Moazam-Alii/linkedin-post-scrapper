34a7ef062e5edd09a69b49ac39033ec25a6f8478
Moazam Ali <amoazam589@gmail.com>
2025-06-01 01:14:44 +0500

    port fixing

--- main.py
+++ main.py
@@ -196,6 +196,8 @@ def oauth2callback():
     flash("✅ Google API credentials saved successfully.", "success")
     return redirect(url_for('start'))
 
+ 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
